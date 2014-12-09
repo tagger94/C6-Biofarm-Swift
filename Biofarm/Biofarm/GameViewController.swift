111fedf1c127da872995ca6c7dce19a9a6c241bb
Voigt <cjvoigt@co1313-06-mac-2.local>
2014-12-09 15:49:37 -0600

    testing skview

--- Biofarm/Biofarm/GameViewController.swift
+++ Biofarm/Biofarm/GameViewController.swift
@@ -29,21 +29,6 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
     }
 
     override func shouldAutorotate() -> Bool {
@@ -66,4 +51,8 @@ class GameViewController: UIViewController {
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
+    @IBAction func ButtonTest(sender: UIButton) {
+        
+    }
+    
 }
