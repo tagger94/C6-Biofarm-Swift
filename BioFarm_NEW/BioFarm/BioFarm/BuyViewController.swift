5b7f5710f6ebd4699ae82aa128d473e0e9da00ea
Jay Staker <jstaker@jstakermacpro13.exnet.iastate.edu>
2015-01-24 16:55:06 -0600

    Added the popover menu for gameplay

---
+++ BioFarm_NEW/BioFarm/BioFarm/BuyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BuyViewController.swift
+//  BioFarm
+//
+//  Created by Jay Staker on 1/24/15.
+//
+//
+
+import Foundation
+import UIKit
+
+class BuyViewController: UIViewController {
+    
+    @IBOutlet var switchGrassButton : UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buySwitchGrass(sender: AnyObject) {
+        
+    }
+    
+}
--- BioFarm_NEW/BioFarm/BioFarm/GameViewController.swift
+++ BioFarm_NEW/BioFarm/BioFarm/GameViewController.swift
@@ -13,6 +13,16 @@ class GameViewController : UIViewController{
     
     @IBOutlet var moneyLabel : UILabel!
     @IBOutlet var startButton : UIButton!
+    @IBOutlet var farm1Button : UIButton!
+    
+    @IBAction func start(sender: AnyObject) {
+        var farmMoney = Money()
+        moneyLabel.text = String(format:"$%.2f", farmMoney.getCash());
+    }
+    
+    func refreshPlay(){
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,15 +33,5 @@ class GameViewController : UIViewController{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func start(sender: AnyObject) {
-        var farmMoney = Money()
-        moneyLabel.text = String(format:"%f", farmMoney.getCash());
-        
-    }
-
-    
-    
-    
     
 }
